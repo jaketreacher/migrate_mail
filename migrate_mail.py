748c185f7d3002b71bd5f9c9fb261396d957f81e
Jake Treacher <git@jaketreacher.com>
2017-11-19 01:37:18 +0800

    Add duplicate checking

--- migrate_mail.py
+++ migrate_mail.py
@@ -1,4 +1,5 @@
 import csv
+import email
 import imaplib
 import shlex
 import time
@@ -9,34 +10,46 @@ def imap_connect(username, password, server, port=993):
 
     return imap
 
-def get_all_mail(imap):
-    typ, data = imap.search(None, 'ALL')
+def get_all_mail(imap, location):
+    typ, data = imap.uid('search',None, 'ALL')
     mail_list = []
-    for num in data[0].split():
-        data = imap.fetch(num, '(FLAGS INTERNALDATE RFC822)')[1]
+    uid_list = data[0].split()
+    length = len(uid_list)
+
+    for idx, uid in enumerate(uid_list):
+        print("Fetching {}... {}/{}".format(location, idx+1, length), end='\r') # Progress indicator
+
+        data = imap.uid('fetch', uid, '(FLAGS INTERNALDATE RFC822)')[1]
         flags = " ".join([flag.decode() for flag in imaplib.ParseFlags(data[0][0])])
         date = imaplib.Internaldate2tuple(data[0][0])
-        
+        message_id = email.message_from_bytes(data[0][1])['Message-ID'] # Used to check for duplicates
+
         mail_dict = {
+            'uid': uid,
             'data': data[0][1],
             'flags': flags,
-            'date': date
+            'date': date,
+            'Message-ID': message_id
         }
 
         mail_list.append(mail_dict)
-    
+
+    if( length > 0 ):
+        print() # Move cursor to next line
+    else:
+        print("Fetching %s... 0/0" % location)
+
     return mail_list
-
-
 
 def copy_mail(from_account, to_account):
     mailboxes = ['"' + shlex.split(item.decode())[-1] + '"' for item in from_account.list()[1]]
 
-    for mailbox in mailboxes:
+    num_mailboxes = len(mailboxes)
+    for mail_index, mailbox in enumerate(mailboxes):
         code, data = from_account.select(mailbox)
-        count = int(data[0])
-        print("{}: {}".format(mailbox, count))
-        if count > 0:
+        total_mail = int(data[0])
+        print("{}: {} mail, {}/{}".format(mailbox, total_mail, mail_index+1, num_mailboxes))
+        if total_mail > 0:
             # Create mailbox on TO if it doesn't exit
             code = to_account.select(mailbox)[0]
             if code == 'NO':
@@ -44,24 +57,26 @@ def copy_mail(from_account, to_account):
                 to_account.select(mailbox)
 
             # Get all mail
-            print("Fetching mail... ")
-            from_mail_list = get_all_mail(from_account)
-            to_mail_list = get_all_mail(to_account)
+            from_mail = get_all_mail(from_account, "source")
+            to_mail = get_all_mail(to_account, "destination")
 
-            # Remove Duplicates - not working
-            # from_mail_list = [ mail for mail in from_mail_list if mail not in to_mail_list]
+            # Remove Duplicates
+            unique_mail = [mail for mail in from_mail \
+                if mail['Message-ID'] not in \
+                    [mail['Message-ID'] for mail in to_mail] \
+            ]
 
-            length = len(from_mail_list)
+            length = len(unique_mail)
             if length > 0:
-                print("Copying mail... ")
-                for idx, mail in enumerate(from_mail_list):
-                    print("{}/{}".format(idx+1, length), end='\r')
+                for idx, mail in enumerate(unique_mail):
+                    print("Copying mail... {}/{}".format(idx+1, length), end='\r')
                     to_account.append(mailbox, mail['flags'], mail['date'], mail['data'])
                 print()
             else:
                 print('No new mail')
 
             to_account.close()
+            print() # new line for formatting
         from_account.close()
 
 def main():
