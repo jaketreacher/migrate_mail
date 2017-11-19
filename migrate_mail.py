35c48b7255f387568e7fcb1db3b0c36d92708661
Jake Treacher <git@jaketreacher.com>
2017-11-19 12:13:03 +0800

    Add function to convert namespaces and separators

--- migrate_mail.py
+++ migrate_mail.py
@@ -10,6 +10,55 @@ def imap_connect(username, password, server, port=993):
 
     return imap
 
+def get_mailbox_info(mailbox_data):
+    data = re.search('(?P<flags>\(.*\)) "(?P<sep>.)" (?P<name>.*)', 
+            mailbox_data[0].decode()).groupdict()
+    sep = data['sep']
+    namespace = data['name'] if (sep == '.') else ''
+    return sep, namespace
+
+def convert_mailbox_format(mailbox_data):
+    data_list = []
+    for box in mailbox_data:
+        data_list.append(
+            re.search('(?P<flags>\(.*\)) "(?P<sep>.)" (?P<name>.*)', 
+            box.decode()).groupdict()
+        )
+    return data_list
+
+def change_namespace(name, old_ns, new_ns, old_sep):
+    name = name.replace('"','')
+    if (name.upper() != "INBOX"):
+        if (old_ns != '') and (new_ns == ''):
+            name = name.replace(old_ns + old_sep, '', 1)
+        if (old_ns == '') and (new_ns != ''):
+            name = new_ns + old_sep + name
+        if (old_ns != '') and (new_ns != ''):
+            name = name.replace(old_ns + old_sep, new_new + old_sep, 1)
+    return '"' + name + '"'
+
+def get_mail_table(from_server, to_server):
+    from_mailbox_data = from_server.list()[1]
+    to_mailbox_data = to_server.list()[1]
+
+    _, from_namespace = get_mailbox_info(from_mailbox_data)
+    new_sep, to_namespace = get_mailbox_info(to_mailbox_data)
+
+    mailbox_list = convert_mailbox_format(from_mailbox_data)
+    mail_table = dict()
+
+    for mailbox in mailbox_list:
+        # Change the namespace and replace the separators
+        mail_table[mailbox['name']] = \
+            change_namespace(
+                mailbox['name'],
+                from_namespace,
+                to_namespace,
+                mailbox['sep']
+            ).replace(mailbox['sep'], new_sep)
+    
+    return mail_table
+
 def get_all_mail(imap, location):
     typ, data = imap.uid('search',None, 'ALL')
     mail_list = []
@@ -45,16 +94,19 @@ def copy_mail(from_account, to_account):
     mailboxes = ['"' + shlex.split(item.decode())[-1] + '"' for item in from_account.list()[1]]
 
     num_mailboxes = len(mailboxes)
+    # mail table used as a hash to convert old mailbox to new format, if required
+    mail_table = get_mail_table(from_account, to_account)
+
     for mail_index, mailbox in enumerate(mailboxes):
         code, data = from_account.select(mailbox)
         total_mail = int(data[0])
         print("{}: {} mail, {}/{}".format(mailbox, total_mail, mail_index+1, num_mailboxes))
         if total_mail > 0:
-            # Create mailbox on TO if it doesn't exit
-            code = to_account.select(mailbox)[0]
+            # Create mailbox on destination if it doesn't exist
+            code = to_account.select(mail_table[mailbox])[0]
             if code == 'NO':
-                to_account.create(mailbox)
-                to_account.select(mailbox)
+                to_account.create(mail_table[mailbox])
+                to_account.select(mail_table[mailbox])
 
             # Get all mail
             from_mail = get_all_mail(from_account, "source")
@@ -70,7 +122,7 @@ def copy_mail(from_account, to_account):
             if length > 0:
                 for idx, mail in enumerate(unique_mail):
                     print("Copying mail... {}/{}".format(idx+1, length), end='\r')
-                    to_account.append(mailbox, mail['flags'], mail['date'], mail['data'])
+                    to_account.append(mail_table[mailbox], mail['flags'], mail['date'], mail['data'])
                 print()
             else:
                 print('No new mail')
